d1617b38b16b74575bf0eb25690187b5d5cf3dbd
mombe090 <yayamombeya090@gmail.com>
2020-12-10 16:04:16 +0000

    debug 3

--- pre-registration/pre-registration-notification-service/src/main/java/io/mosip/preregistration/notification/dto/JsonString.java
+++ pre-registration/pre-registration-notification-service/src/main/java/io/mosip/preregistration/notification/dto/JsonString.java
@@ -1,10 +1,12 @@
 package io.mosip.preregistration.notification.dto;
 
-import lombok.Builder;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class JsonString {
     private String value;
     private String langCode;
