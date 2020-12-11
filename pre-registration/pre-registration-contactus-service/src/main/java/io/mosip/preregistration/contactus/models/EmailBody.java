6d99f97b1b26b149f3d7470e021559a5a81b5e95
mombe090 <yayamombeya090@gmail.com>
2020-12-11 11:09:57 +0000

    Typo corrected

--- pre-registration/pre-registration-contactus-service/src/main/java/io/mosip/preregistration/contactus/models/EmailBody.java
+++ pre-registration/pre-registration-contactus-service/src/main/java/io/mosip/preregistration/contactus/models/EmailBody.java
@@ -1,12 +1,14 @@
 package io.mosip.preregistration.contactus.models;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class EmailBody {
     private String object;
     private String name;
